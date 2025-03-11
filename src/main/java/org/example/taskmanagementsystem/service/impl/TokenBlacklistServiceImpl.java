c5900bff981bb05bb862c2694caa9342ebf5dad0
Zhandos04 <nurbekulyzhandos@gmail.com>
2025-03-11 15:25:57 +0500

    some changes

--- src/main/java/org/example/taskmanagementsystem/service/impl/TokenBlacklistServiceImpl.java
+++ src/main/java/org/example/taskmanagementsystem/service/impl/TokenBlacklistServiceImpl.java
@@ -1,36 +1,25 @@
 package org.example.taskmanagementsystem.service.impl;
 
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.example.taskmanagementsystem.service.TokenBlacklistService;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-@RequiredArgsConstructor
 public class TokenBlacklistServiceImpl implements TokenBlacklistService {
-
-    private final RedisTemplate<String, String> redisTemplate;
-
+    private final ConcurrentHashMap<String, Long> tokenBlacklist = new ConcurrentHashMap<>();
     @Override
     public void addTokenToBlacklist(String token, Date expirationTime) {
-        long timeToLiveMillis = expirationTime.getTime() - System.currentTimeMillis();
-        if (timeToLiveMillis > 0) {
-            long ttlSeconds = timeToLiveMillis / 1000;
-            redisTemplate.opsForValue().set(token, "blacklisted", ttlSeconds, TimeUnit.SECONDS);
+        long timeToLive = expirationTime.getTime() - System.currentTimeMillis();
+        if (timeToLive > 0) {
+            tokenBlacklist.put(token, expirationTime.getTime());
         }
     }
     @Override
     public boolean isTokenBlacklisted(String token) {
-        return Boolean.TRUE.equals(redisTemplate.hasKey(token));
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("Redis Host: " + redisTemplate.getConnectionFactory().getConnection().ping());
+        Long expirationTime = tokenBlacklist.get(token);
+        return expirationTime != null && expirationTime > System.currentTimeMillis();
     }
 
 }
